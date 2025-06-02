c0d1afe44b58e513b43b5aaa10d6222795d26fe9
sahasradoddi <sahasradoddi@gmail.com>
2025-06-02 18:10:10 +0530

    added ('/patient/{patient_id') view point with a Httpxustom exception and implemented Path()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
 import json
 
 def load_dataset():
@@ -19,3 +19,10 @@ def about():
 def view():
     data=load_dataset()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str=Path(...,description="ID of the patient should be in DB",example="P0001")):
+    data=load_dataset()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=400,details="patient not found in the database")
